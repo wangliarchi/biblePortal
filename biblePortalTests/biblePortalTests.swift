dc314573c24c58b93d8972f607eace054121c721
Li Wang <leahwang@rndsupport.net>
2015-08-27 20:10:05 -0700

    Initial Commit

---
+++ biblePortalTests/biblePortalTests.swift
@@ -0,0 +1,36 @@
+//
+//  biblePortalTests.swift
+//  biblePortalTests
+//
+//  Created by Li Wang on 8/27/15.
+//  Copyright (c) 2015 OU. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class biblePortalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
